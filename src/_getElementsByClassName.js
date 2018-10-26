6aee86298fffca4758abd0e9b5463b009972cfa7
Aaron Deane <aaronmdeane@gmail.com>
2018-10-25 17:03:52 -0700

    refactored getElementsByClassName to simplify instead of using regex

--- src/_getElementsByClassName.js
+++ src/_getElementsByClassName.js
@@ -5,20 +5,24 @@
 
 // But instead we're going to implement it from scratch:
 var _getElementsByClassName = function(className) {
-  var nodeArr = Array.prototype.slice.apply(document.body.childNodes); //parse childNodes object into array
-  var classRegex = new RegExp(className, 'ig'); // create regex from className to search each node
-  var results = []; //variable to store DOM elements that match className
-  var childrenByClass = function(arr) {
-    arr.forEach(function(elem) {//iterate through the childNodes array
-      if(classRegex.test(elem.classList) || classRegex.test(elem)) {//check element classList values against className regex
-        results.push(elem);//push the matching element to the results array
-      }
-      if(elem.nodeName !== '#text' && elem.childNodes.length > 0) {//skip text nodes or others without children
-        var elemArr = Array.prototype.slice.apply(elem.childNodes);//parse childNode object into array
-        return childrenByClass(elemArr);//use recursion to iterate through childNodes
+  var nodeArr = document.body; //parse childNodes object into array
+  var elementsMatchingClass = []; //variable to store DOM elements that match className
+  
+  var childrenByClass = function(elem) {
+    if(elem.classList.contains(className)) {//check element classList values against className regex
+      elementsMatchingClass.push(elem);//push the matching element to the results array
+    }
+      
+    if(elem.childNodes.length > 0) {//check to see if the element has children
+      var elemArr = elem.childNodes;
+      elemArr.forEach(function(node) {//iterate through the childNodes 
+        if(node.id !== 'mocha' && node.nodeName !== '#text') {
+          return childrenByClass(node);//use recursion to iterate through childNodes
         }
       });
+    }
   }
-  childrenByClass(nodeArr);
-  return results;
+  
+  childrenByClass(nodeArr);//call the method on our document body
+  return elementsMatchingClass;//return the results of the method
 };
